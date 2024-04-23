c7453caaa313275acb764ef85ef664f3bfa8de87
Luis Felipe Tavera <luis.tavera@newshore.es>
2024-04-22 19:58:31 -0500

    final changes

---
+++ body-container/header-web/body-web.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'body-web',
+  standalone: true,
+  templateUrl: './body-web.component.html'
+})
+export class BodyWebComponent {
+  
+}
