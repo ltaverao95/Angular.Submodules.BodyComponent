d86069173934b153ee23750174d260898a3a4275
natalia.tavera <natalia.tavera@flyrlabs.com>
2024-04-24 08:03:01 -0500

    paragraph added and set color

--- body-container/header-web/body-web.component.ts
+++ body-container/header-web/body-web.component.ts
@@ -3,7 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'body-web',
   standalone: true,
-  templateUrl: './body-web.component.html'
+  templateUrl: './body-web.component.html',
+  styles: [
+    `
+      .body {
+        &__p {
+          color:red ;
+        }
+        &--green{
+          color: green,
+        }
+      }
+    `,
+  ],
 })
 export class BodyWebComponent {
   
